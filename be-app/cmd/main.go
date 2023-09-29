7cb4d28cd1e2abd742e83fc993c086b7c6b7a4cf
Christopher Rivera <christopherrivera384@gmail.com>
2023-09-29 19:21:59 -0400

    Wroking on route and added a .env

--- be-app/cmd/main.go
+++ be-app/cmd/main.go
@@ -1,24 +1,59 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"net/smtp"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	client, err := smtp.Dial("smtp.gmail.com:587")
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
+	r.Post("/", sendEmail)
+
+	err := godotenv.Load("./.env")
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error loading .env file ", err)
 	}
 
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT not found within the environment")
+	}
+
+	fmt.Println("Port listening on port :", portString)
+	log.Fatal(http.ListenAndServe(portString, r))
+}
+
+func sendEmail(w http.ResponseWriter, r *http.Request) {
 	from := "Christopherrivera384@gmail.com"
+	pass := os.Getenv("APP_PASSWORD")
 	to := "Christopherrivera384@gmail.com"
 
-	message := []byte("Subject: Test email \n\n This is a test email")
+	body := "Subject: Hello there!\r\n" +
+		"This email is the body."
 
-	err = client.Mail(from)
+	smtpServer := "smtp.gmail.com"
+	port := ":587"
+	auth := smtp.PlainAuth("", from, pass, smtpServer)
+
+	err := smtp.SendMail(smtpServer+port, auth, from, []string{to}, []byte(body))
+
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Error sending email ", err)
+		return
 	}
+
+	fmt.Println("Sent Successfully!")
 }
--- be-app/go.mod
+++ be-app/go.mod
@@ -1,3 +1,8 @@
-module crisncris0000/be-app
+module github.com/crisncris0000/Portfolio/be-app
 
 go 1.20
+
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
